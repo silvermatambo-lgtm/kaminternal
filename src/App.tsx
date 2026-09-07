280afe2ed5347d2cf40d2cea5f89ccfa6e56af11
webdevproproject <silvermatambo@gmail.com>
2026-09-07 14:38:52 +0200

    Convert KAM site to multi-page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,30 @@
-import Navbar from './components/Navbar'; import Footer from './components/Footer'; import Home from './pages/Home'; import FloatingWhatsApp from './components/FloatingWhatsApp'; import PWAInstall from './components/PWAInstall'; import ScrollToTop from './components/ScrollToTop';
-export default function App(){return <><Navbar/><Home/><Footer/><FloatingWhatsApp/><PWAInstall/><ScrollToTop/></>}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import FloatingWhatsApp from './components/FloatingWhatsApp';
+import PWAInstall from './components/PWAInstall';
+import ScrollToTop from './components/ScrollToTop';
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+      <FloatingWhatsApp />
+      <PWAInstall />
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+}
