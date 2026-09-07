7a3ab371630bae5afc96a66229d82656f3875000
webdevproproject <silvermatambo@gmail.com>
2026-09-07 14:59:29 +0200

    Fix mobile homepage fallback routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,21 +11,41 @@ import FloatingWhatsApp from './components/FloatingWhatsApp';
 import PWAInstall from './components/PWAInstall';
 import ScrollToTop from './components/ScrollToTop';
 
-export default function App(){
+function RouteReset(){
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+  return null;
+}
+
+function AppRoutes(){
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <main className="min-h-[60vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
       <Footer />
       <FloatingWhatsApp />
       <PWAInstall />
       <ScrollToTop />
+      <RouteReset />
+    </>
+  );
+}
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
